7108b3c61e1b10b882f1ae4e1129396554b0298a
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-08-02 15:20:24 +0000

    [Add] userinfoへんしゅう&投稿varidate追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
     #@books = @user.books.page(params[:page])
+  end
+  
+  def index
+    @book = Book.new
+    @user = current_user
+  	@users = User.all
   end
 
   def edit
@@ -16,6 +23,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
