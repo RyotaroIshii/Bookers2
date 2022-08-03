65ea65d0badd42a2129b7a45c7d81feb120e352b
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-08-03 19:02:13 +0000

    [Add] userinfo＆show・indexのためのcon記述（途中）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,31 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:notice] = 'You have created book successfully.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   def show
+    @books = Book.find(params[:id])
+    @book = Book.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    redirect_to books_path unless current_user == @book.user
   end
 
 end
