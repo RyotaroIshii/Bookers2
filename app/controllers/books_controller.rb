3df190b965da4690bb86460c6a5f639609fde286
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-08-06 10:36:23 +0000

    [Add] book/conにストロングパラメーター追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,16 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(book)
       flash[:notice] = 'You have created book successfully.'
     else
       @books = Book.all
       render :index
     end
-    
+
   end
 
   def show
@@ -29,6 +26,11 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     redirect_to books_path unless current_user == @book.user
+  end
+
+  private
+  def book_params
+      params.require(:book).permit(:title, :body)
   end
 
 end
