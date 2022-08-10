7bdcf10ec4527b145e0b1efa01412609c9fdbde4
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-08-10 19:02:09 +0000

    [Add] book/update実装&migrate訂正,それに伴うコマンド修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,15 +27,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.user == current_user
       render :edit
-
     else
       redirect_to books_path
     end
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = 'You have updated book successfully.'
+    else
+      render :edit
+    end
+  end
+
   private
   def book_params
-      params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :opinion)
   end
 
 end
